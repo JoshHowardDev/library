b2a38b7d924448becbfa0173d85fc4b37a7da224
JoshH831 <joshhowardspring@gmail.com>
2022-09-23 11:20:20 -0400

    Add functionality to populate page with initial books.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+bookShelf = document.querySelector('.shelf')
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -16,3 +18,19 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read));
 }
+
+function initialLibrary() {
+    addBookToLibrary('The Great Gatsby', 'F Scott Fitzgerald', 218, true);
+    addBookToLibrary('A Game of Thrones', 'George RR Martin', 694, true);
+    addBookToLibrary('The Phantom Tollbooth', 'Norton Juster', 255, true);
+}
+initialLibrary();
+
+console.log(myLibrary)
+
+myLibrary.forEach(book => {
+    let bookCard = document.createElement('div');
+    bookCard.classList.add('book')
+    bookCard.textContent = book.title
+    bookShelf.appendChild(bookCard)
+});

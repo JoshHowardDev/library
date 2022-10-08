93d8d625118221b230418ceccee71e15cf1257d9
JoshH831 <joshhowardspring@gmail.com>
2022-10-08 09:48:13 -0400

    Change book factor function to book class.

--- script.js
+++ script.js
@@ -8,13 +8,15 @@ const buttonsDiv = document.querySelector('.buttonsDiv')
 
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book{
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 
-    this.info = () => {
+    info() {
         let readStr = ''
         read ? readStr = 'has been read' : 'not read yet'
         return(`${this.title} by ${this.author}, ${this.pages} pages, ${readStr}.`)
